41d03f9fd34c046eec7c5874caaaab582566bd6e
dev-gursewak <gursewak@duck.com>
2024-10-20 03:14:49 -0600

    build function for make list and deleting list

--- myscript.js
+++ myscript.js
@@ -1,10 +1,27 @@
+//retived three element from the element which is input, ol, and sumbit button
 const shoppingList = document.querySelector("#shoppingList");
 const visibleList = document.querySelector(".orderedList");
+const btn = document.querySelector("#btn");
 
+
+//created li, span and delete to append into visibleList
 const list = document.createElement("li");
-list.textContent = shoppingList.value;
+const span = document.createElement("span");
+const deleteButton = document.createElement("button");
 
-visibleList.appendChild(list)
 
+//function which runs when submit button is clicked
+btn.onclick = function(){
+    let currentValue = shoppingList.value;
+    shoppingList.value = '';
+    span.textContent = `${currentValue} `;
+    deleteButton.textContent = "delete";
+    list.appendChild(span);
+    list.appendChild(deleteButton);
+}
 
-alert(shoppingList.value);
+visibleList.appendChild(list);
+
+deleteButton.onclick = function() {
+    deleteButton.parentNode.remove();
+}
